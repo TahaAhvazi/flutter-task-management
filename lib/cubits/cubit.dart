d974c3e1e332b9260621fa1bb29533fbdef1b0e6
Taha <tahax.dev@gmail.com>
2022-07-08 19:23:57 +0430

    Add cubit file in the cubit directory

---
+++ lib/cubits/cubit.dart
@@ -0,0 +1,5 @@
+export 'task_filtred/task_filtred_cubit.dart';
+export 'task_search/task_search_cubit.dart';
+export 'task_list/task_list_cubit.dart';
+export 'active_task_count/active_task_count_cubit.dart';
+export 'task_filter/task_filter_cubit.dart';
